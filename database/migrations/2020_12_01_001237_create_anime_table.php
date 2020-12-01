ae4919a8e8cff47fe062ff699befc84aa7487b89
toloza011 <estebantoloza1998@gmail.com>
2020-11-30 21:23:10 -0300

    migracion anime

---
+++ database/migrations/2020_12_01_001237_create_anime_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anime', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('sinopsis');
+            $table->string('portada');
+            $table->string('trailer_url')->nullable();
+            $table->date('estreno');
+            $table->integer('estado');
+            $table->float('calificacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anime');
+    }
+}
